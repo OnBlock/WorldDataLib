330af9480dc49ddf2ff910210c7db96f7fee8571
Indigo Amann <indigoa.coding@gmail.com>
2019-10-07 00:46:57 -0700

    Data can be saved, Server and Client

--- src/main/java/io/github/indicode/fabric/worlddata/WorldDataLib.java
+++ src/main/java/io/github/indicode/fabric/worlddata/WorldDataLib.java
@@ -1,5 +1,7 @@
 package io.github.indicode.fabric.worlddata;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +10,10 @@ import java.util.List;
  */
 public class WorldDataLib {
     private static List<WorldIOCalback> callbacks = new ArrayList<>();
-    public static void addSaveCallback(WorldIOCalback callback) {
+    public static void addIOCallback(WorldIOCalback callback) {
         callbacks.add(callback);
+    }
+    public static ImmutableList<WorldIOCalback> getIOCallbacks() {
+        return ImmutableList.copyOf(callbacks);
     }
 }
---
+++ src/main/java/io/github/indicode/fabric/worlddata/mixin/WorldSaveHandlerMixin.java
@@ -0,0 +1,41 @@
+package io.github.indicode.fabric.worlddata.mixin;
+
+import com.mojang.datafixers.DataFixer;
+import io.github.indicode.fabric.worlddata.WorldDataLib;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldSaveHandler;
+import net.minecraft.world.level.LevelProperties;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(WorldSaveHandler.class)
+public class WorldSaveHandlerMixin {
+    private MinecraftServer server;
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void setServer(File file_1, String string_1, MinecraftServer minecraftServer_1, DataFixer dataFixer_1, CallbackInfo ci) {
+        server = minecraftServer_1;
+    }
+    @Inject(method = "saveWorld(Lnet/minecraft/world/level/LevelProperties;Lnet/minecraft/nbt/CompoundTag;)V", at = @At("RETURN"))
+    private void onSaveWorld(LevelProperties levelProperties_1, CompoundTag compoundTag_1, CallbackInfo ci) {
+        WorldDataLib.getIOCallbacks().forEach(callback -> {
+            System.out.println(callback);
+            System.out.println(worldDir);
+            System.out.println(server.isDedicated() ? worldDir.getParentFile() : MinecraftClient.getInstance().runDirectory);
+            callback.onWorldSave(worldDir, server.isDedicated() ? worldDir.getParentFile() : MinecraftClient.getInstance().runDirectory);
+        });
+    }
+    @Final
+    @Shadow
+    private File worldDir;
+}
