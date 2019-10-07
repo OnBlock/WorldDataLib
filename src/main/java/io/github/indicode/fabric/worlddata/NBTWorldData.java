e5f6eea5b83a7a718aeda24c2370e4c12a13e3a2
Indigo Amann <indigoa.coding@gmail.com>
2019-10-07 00:05:02 -0700

    Save method for NBTWorldData

--- src/main/java/io/github/indicode/fabric/worlddata/NBTWorldData.java
+++ src/main/java/io/github/indicode/fabric/worlddata/NBTWorldData.java
@@ -1,8 +1,11 @@
 package io.github.indicode.fabric.worlddata;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author Indigo Amann
@@ -10,19 +13,47 @@ import java.io.File;
 public abstract class NBTWorldData implements WorldIOCalback, NBTSavableData {
     protected boolean backup;
 
-    public NBTWorldData(boolean backup) {
-
+    public NBTWorldData(boolean backup, boolean crashOnCorrupt) {
+        this.backup = backup;
     }
 
-    public abstract File getSaveFile(File worldDirectory, File rootDirectory);
-    
+    public abstract File getSaveFile(File worldDirectory, File rootDirectory, boolean backup);
+
     @Override
     public void onWorldSave(File worldDirectory, File rootDirectory) {
-        File file = getSaveFile(worldDirectory, rootDirectory);
+        File backupFile = getSaveFile(worldDirectory, rootDirectory, true);
+        File file = getSaveFile(worldDirectory, rootDirectory, false);
+        if (file.exists()) {
+            if (backupFile.exists()) {
+                if (!backupFile.delete()) throw new RuntimeException(new IOException("Could not delete backup file: " + backupFile.getAbsolutePath()));
+            }
+            if (!backupFile.getParentFile().isDirectory()) {
+                if (!backupFile.mkdirs()) throw new RuntimeException(new IOException("Could not create directory " + backupFile.getAbsolutePath() + " for data backup."));
+            }
+            if (!file.renameTo(backupFile)) throw new RuntimeException(new IOException("Could not rename data file " + file.getAbsolutePath() + " to backup file: " + backupFile.getAbsolutePath()));
+        }
+        if (!file.getParentFile().isDirectory()) {
+            if (!file.mkdirs()) throw new RuntimeException(new IOException("Could not create directory " + file.getAbsolutePath() + " for data."));
+        }
+        if (!file.isFile()) {
+            try {
+                if (!file.createNewFile()) throw new RuntimeException(new IOException("Could not create data file " + file.getAbsolutePath()));
+            } catch (IOException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+        CompoundTag tag = toNBT(new CompoundTag());
+        if (tag == null) throw new RuntimeException(new NullPointerException("Null tag was passed for " + file.getAbsolutePath()));
+        try {
+            NbtIo.writeCompressed(tag, new FileOutputStream(file));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     @Override
     public void onWorldLoad(File worldDirectory, File rootDirectory) {
-        File file = getSaveFile(worldDirectory, rootDirectory);
+        File backupFile = getSaveFile(worldDirectory, rootDirectory, true);
+        File file = getSaveFile(worldDirectory, rootDirectory, false);
     }
 }
